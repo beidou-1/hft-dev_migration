6471e27b2c44616aefb9510cf6cbf0da10d180d4
Ubuntu <ubuntu@ip-172-31-0-52.ap-northeast-1.compute.internal>
2026-05-18 19:41:03 +0000

    new

--- Build_bybit/exchangelink-hft-sdk/third-party/fmtlog/CMakeFiles/fmtlog-static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libfmtlog-static.a"
-)
--- Build_okex/exchangelink-hft-sdk/third-party/sbepp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/hft-dev_migration/exchangelink-hft-sdk/third-party/sbepp
-# Build directory: /home/ubuntu/hft-dev_migration/Build_okex/exchangelink-hft-sdk/third-party/sbepp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("sbepp")
