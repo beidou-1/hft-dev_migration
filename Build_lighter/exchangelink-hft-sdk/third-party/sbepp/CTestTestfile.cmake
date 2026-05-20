367554445e9808b8701596901c62169c310fdb16
pete <pete@pete.com>
2026-05-20 18:25:30 +0800

    new

--- Build_lighter/exchangelink-hft-sdk/third-party/sbepp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/e/jiacang/remote_wbd/hft-dev_migration/exchangelink-hft-sdk/third-party/sbepp
-# Build directory: /mnt/e/jiacang/remote_wbd/hft-dev_migration/Build_lighter/exchangelink-hft-sdk/third-party/sbepp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("sbepp")
