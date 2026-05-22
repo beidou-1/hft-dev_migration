a7c6d118f73616b0423983323fb6900cc4d48172
Ubuntu <ubuntu@ip-172-31-37-206.ap-northeast-1.compute.internal>
2026-05-22 11:49:01 +0000

    new

--- Build_bitunix/exchangelink-hft-sdk/third-party/sbepp/sbepp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/hft-dev_migration/exchangelink-hft-sdk/third-party/sbepp/sbepp
-# Build directory: /home/ubuntu/hft-dev_migration/Build_bitunix/exchangelink-hft-sdk/third-party/sbepp/sbepp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
